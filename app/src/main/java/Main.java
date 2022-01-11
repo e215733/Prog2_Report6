a8014d4dca7eb0bf7566a29c90df675385717172
Yuuto Chinen <e215733@ie.u-ryukyu.ac.jp>
2022-01-11 09:20:17 +0900

    Mainクラスの実装

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import jp.ac.uryukyu.ie.e215733.*;
+
+/**
+ * ゲーム実行を行うクラス。
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        PlayGame.GamePlay(26);
+
+    }
+    
+}
